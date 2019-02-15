bed3ec962cc56114edf8b600841241a798b97ae0
Ahmad Game <ahmad.game@lime.tech>
2019-02-15 13:27:04 +0100

    age code should live in the bio.js

--- src/_bio/bio.js
+++ src/_bio/bio.js
@@ -1,7 +1,15 @@
 import './bio.scss';
 import '../img/Game.png';
 
-import setAge from '../js/age';
 import init from '../js/init';
+
+function setAge() {
+    const ageDifMs = Date.now() - new Date(1985, 6, 26).getTime();
+    const ageDate = new Date(ageDifMs); // milliseconds from epoch
+    const ageDiv = document.querySelector('#age');
+    if (ageDiv) {
+        ageDiv.textContent = Math.abs(ageDate.getUTCFullYear() - 1970);
+    }
+};
 
 init([setAge]);
