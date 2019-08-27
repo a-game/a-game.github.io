f14de6754ef98576cc6a76969b0c06cb25f28f78
Ahmad Game <me@ahmadgame.com>
2019-08-27 14:27:33 +0200

    Updates

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-import Header from "./header";
-import Layout from "./layout";
-import SEO from "./seo";
-import { Link, ListLink } from "./link";
-import Contact from "./contact";
-
-export { Header, Layout, SEO, Link, ListLink, Contact };
