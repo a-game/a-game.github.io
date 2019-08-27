7cda3b749b9efd11396db1e868e535d1f7b4b601
Ahmad Game <me@ahmadgame.com>
2019-08-27 14:31:43 +0200

    test without token

--- deploy.js
+++ deploy.js
@@ -1,14 +1,14 @@
 const ghpages = require("gh-pages");
-const token = process.env.ACCESS_TOKEN;
-console.log(token);
-const repo = `https://${token}@github.com/a-game/a-game.github.io.git`;
-console.log(repo);
+// const token = "ff877e068b9b863420216a06e52b5048a893110f"; //process.env.ACCESS_TOKEN;
+// console.log(token);
+// const repo = `https://${token}@github.com/a-game/a-game.github.io.git`;
+// console.log(repo);
 
 ghpages.publish(
   "public",
   {
     branch: "master",
-    repo: repo,
+    repo: "https://github.com/a-game/a-game.github.io.git",
   },
   e => {
     const msg = "Deploy OK!";
