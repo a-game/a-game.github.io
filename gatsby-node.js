12461b1e8958116fee89447d53752a45a7a93ed8
Ahmad Game <me@ahmadgame.com>
2021-03-16 10:08:29 +0100

    Migrating Remark to MDX

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,7 +7,7 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
 
   const result = await graphql(`
     {
-      allMarkdownRemark(
+      allMdx(
         sort: { order: DESC, fields: [frontmatter___date] }
         limit: 1000
       ) {
@@ -28,7 +28,7 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
     return;
   }
 
-  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+  result.data.allMdx.edges.forEach(({ node }) => {
     createPage({
       path: node.frontmatter.path,
       component: postTemplate,
--- src/templates/postTemplate.jsx
+++ src/templates/postTemplate.jsx
@@ -1,18 +1,16 @@
 import React from "react";
 import { graphql } from "gatsby";
+import { MDXRenderer } from "gatsby-plugin-mdx";
 import { Layout, SEO } from "../components";
 
 const PostTemplate = ({ data }) => {
-  const { frontmatter, html } = data.post;
+  const { frontmatter, body } = data.post;
   return (
     <Layout>
       <SEO title={frontmatter.title} description={frontmatter.description} />
       <article>
         <i className="createdDate">{frontmatter.date}</i>
-        <div
-          className="blog-post-content"
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
+        <MDXRenderer className="blog-post-content">{body}</MDXRenderer>
       </article>
     </Layout>
   );
@@ -22,8 +20,8 @@ export default PostTemplate;
 
 export const pageQuery = graphql`
   query($path: String!) {
-    post: markdownRemark(frontmatter: { path: { eq: $path } }) {
-      html
+    post: mdx(frontmatter: { path: { eq: $path } }) {
+      body
       frontmatter {
         date(formatString: "MMMM DD, YYYY")
         path
