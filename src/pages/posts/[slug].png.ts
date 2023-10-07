d3d9808b380c27c29ad8d9b5b58449978096e6ae
Ahmad Game <me@ahmadgame.com>
2023-10-07 22:24:32 +0200

    Cleanup (#19)

--- src/pages/posts/[slug].png.ts
+++ src/pages/posts/[slug].png.ts
@@ -1,13 +1,12 @@
 import type { APIRoute } from "astro";
 import { getCollection, type CollectionEntry } from "astro:content";
 import { generateOg } from "util/generate-og";
-import { ROOT_SLUG } from "util/posts";
 
 export async function getStaticPaths() {
   const posts = await getCollection("posts", ({ data }) => !data.draft);
 
   return posts.map((post) => ({
-    params: { slug: post.slug === ROOT_SLUG ? undefined : post.slug },
+    params: { slug: post.slug },
     props: post,
   }));
 }
