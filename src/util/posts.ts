fc4afc36017e26846519a8af6f5f0c95b30db149
Ahmad Game <me@ahmadgame.com>
2023-10-07 22:23:36 +0200

    Move post root to an astro component (#20)

--- src/util/posts.ts
+++ src/util/posts.ts
@@ -1,17 +1,13 @@
 import { getCollection } from "astro:content";
 
-export const ROOT_SLUG = "index";
-
 export async function getPosts({
   onlyFeatured = false,
   includeDrafts = false,
-  includeRoot = false,
   unsorted = false,
 } = {}) {
   const posts = await getCollection(
     "posts",
-    ({ slug, data: { draft = true, featured = false } }) => {
-      if (!includeRoot && slug === ROOT_SLUG) return false;
+    ({ data: { draft = true, featured = false } }) => {
       if (!includeDrafts && draft) return false;
       if (onlyFeatured && !featured) return false;
       return true;
